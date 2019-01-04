47ce9894d21886e538ec9679d3defbe04ff7caf4
Drew Levitt <drew.levitt@gmail.com>
2019-01-04 11:14:03 -0800

    refine flyTo timing, zoom, number of geodesic segments

---
+++ js/entries.js
@@ -0,0 +1,8 @@
+var entries = [
+    {latlng: [37.840455, -122.257520], date: 'December 27, 2018', title: 'This is not a test', location: 'Oakland, CA, USA', zoom: 13 },
+    {latlng: [31.12819929911196, -238.24676513671875],  date: 'December 20, 1995', zoom: 11 },
+    {latlng: [30.987027960280326, -238.46649169921875], date: 'December 25, 1995', zoom: 15 },
+    {latlng: [31.230417393130743, -238.47747802734375], date: 'January 2, 1996', zoom: 13 },
+    {latlng: [31.235114421248575, -238.63128662109375], date: 'January 11, 1996', zoom: 12 },
+    {latlng: [31.340735782189476, -238.41979980468747], date: 'January 23, 1996', zoom: 14 }
+];
