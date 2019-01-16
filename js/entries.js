f783c489cff49c49cc1049456b29b68a5d453a6a
Drew Levitt <drew.levitt@gmail.com>
2019-01-15 19:22:33 -0800

    rework the flyTo functionality to use latLngBounds

--- js/entries.js
+++ js/entries.js
@@ -1,6 +1,6 @@
 var entries = [
-    {latlng: [37.840455, -122.257520], date: 'December 27, 2018', title: 'This is not a test', location: 'Oakland, CA, USA', zoom: 13, url: "https://www.schemebasedadventure.com/2018/12/27/this-is-not-a-test/" },
-    {latlng: [31.12819929911196, -238.24676513671875],  date: 'December 20, 1995', zoom: 11, url: "https://alt.org/nethack/" },
+    {latlng: [37.840455, -122.257520], date: 'December 27, 2018', title: 'This is not a test', location: 'Oakland, CA, USA', url: "https://www.schemebasedadventure.com/2018/12/27/this-is-not-a-test/", corner1: [37.847080, -122.310148], corner2: [37.791333, -122.212991] },
+    {latlng: [31.12819929911196, -238.24676513671875],  date: 'December 20, 1995', zoom: 11, url: "https://alt.org/nethack/", corner1: [31.479148, -239.012087], corner2: [30.735033, -237.85215] },
     {latlng: [30.987027960280326, -238.46649169921875], date: 'December 25, 1995', zoom: 15, url: "https://xkcd.com/" },
     {latlng: [31.230417393130743, -238.47747802734375], date: 'January 2, 1996', zoom: 13, url: "https://www.mcsweeneys.net/" }
 ];
