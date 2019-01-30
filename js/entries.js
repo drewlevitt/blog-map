d45464cff1b41811ee3db3fff9b821d11a0c9791
Drew Levitt <drew.levitt@gmail.com>
2019-01-30 18:57:33 -0500

    add support for wolf-runs paths between markers, via Google linestrings

--- js/entries.js
+++ js/entries.js
@@ -5,33 +5,45 @@ var entries = [
      location: 'Oakland, CA, USA',
      url: 'https://www.schemebasedadventure.com/2018/12/30/this-was-our-oakland/',
      corner1: [37.847080, -122.310148],
-     corner2: [37.791333, -122.212991] },
+     corner2: [37.791333, -122.212991],
+     line: '' },
     {latlng: [37.742698, -122.432150],
      date: 'January 18, 2019',
-     title: 'Tomorrow to fresh woods, and pastures new.',
+     title: 'Tomorrow to fresh woods, and pastures new',
      location: 'San Francisco, CA, USA',
      url: 'https://www.schemebasedadventure.com/2019/01/17/tomorrow-to-fresh-woods-and-pastures-new/',
      corner1: [37.710129, -122.507835],
-     corner2: [37.807414, -122.385118] },
+     corner2: [37.807414, -122.385118],
+     line: "ku}eFpkeiVyJoHeAhCU`AOx@M`AAl@@VXhAb@`Bz@pCj@tBDHRJh@xB|@fEf@lBb@jAr@zAdAjBx@lA~AbBjA~@tBfAtD`A`BVbFl@zDb@~JjA`C\\ZVLFrEr@bFv@bBZhAP~Dl@~B^r@JHHPFp@LRDnB`@f@Pp@`@n@l@b@p@JRZz@TnAHxAExAq@nFWfBaB`NaAtHAX@RDV_@`Cm@~D]dC_BrKMdAK`BElC@|ANzB`@hDr@zEp@nEHNDJN~@LnAFbBCbCSjCe@xBw@|BgAdCw@tBUx@UhAQhBEfABhBHdAV~Aj@lBdAfD`@fBV|AbBbM\\jC@RI|@VjA`A|G~AjM^dFD^FhAl@jO@bB@lDEtEE~BNlEZhDP|AJr@`@zCj@zEx@bFJ~@f@vBn@tCd@xAf@hBd@dCj@`E\\vDZhGTlHl@jQn@`N`AvNrJrnAhBrU`@zD\\bC^tBd@vBf@lBx@hCnAbDdOb[`BfD~AhDz@vAlAbBhArAj@h@rElDnAbAfAdAlBpBzCjDdCrC|B~Bl@bA^^bDnDvB`CdIrJzJtKlK`LxU~VjDzDrDvDnIrJhGxGtI`JtD~D~DjE^\\vApAhApAdBlBlC`DlBbCx@pA\\h@h@hAJXJT`@t@lCtFrAxBp@bAPThH|JxFjIpP~UpAlBlApBzBbE~BtEdAhBv@fAj@j@LH`@XlAn@|Ah@|ATtAD`BE`BSr@Mh@MpBw@rAk@|@[\\Qn@QfB[f@GrAGdKe@zAGv@A|AGdH]x@@fAD~@LhAZ~@XtAr@bAj@pAj@r@Vp@Rv@HdADnAGxAYrAi@hAy@v@y@dBeC`AyAp@y@t@s@fAq@hAa@v@OrAM`GWdHYr@AzA@x@FLRDDNDtD`AjAZDFd@PlA\\b@Rh@f@Zl@Ph@LPj@zFXnBjAjFJt@BjA@rEHbMJfRJ|SRdVDlB@TXEzH]|DQ`@?XBlCr@bG~Al@dUVzLd@rS" },
     {latlng: [37.617948, -122.391691],
      date: 'January 23, 2019',
      title: 'We made it! (?)',
      location: 'SFO, San Francisco, CA, USA',
      url: 'https://www.schemebasedadventure.com/2019/01/23/we-made-it/',
      corner1: [37.604199, -122.410133],
-     corner2: [37.633354, -122.356803] },
+     corner2: [37.633354, -122.356803],
+     line: "crjeFzogjVKwE`DO?O`EOlAGBRNYBW`GgMDKLKJEb@IJ@l@UN?`Bb@LDBDFGnEsE`@i@JWFWXs@ZYH[VmGDaAv@P`@JPHRNDYVi@f@w@h@c@\\IB@F@Lv@Vr@Zd@r@j@^N^D`BHj@H?B?F@DHDHG@A^RTFPANCVQRMBZJh@Xz@V`@f@ZZFZBd@BFcB?aCEiEGcCOcDSqCa@uDKe@MyAIuBA{ABqAEa@?IAQDcGA{@IaBEs@EOOUQuBc@wCWmAOg@Ao@CSYaA{AiF{EcP]cBM{AEgA@{@H}ALcA\\aBj@{A^u@z@gAlAeAtBgAzBeATUfAe@v@YfA[fCi@tE_AhHiBn@@h@S`@MlIcCbGeBnEuAfCw@~Cy@bFkAzNgCbE}@nA_@hAc@rAo@|ByAdHuFn@a@xAu@bBs@x@WtBe@|B]`TuCpJkArDi@~WsD|eA_Opj@yHzNyBlBSrAGtCAfAD`BJnAPfARnBf@zBr@xBx@nBx@xBbAbEzBdC|AvDpCzBhBvBpBpP|O|ErEjBrBhB`ChA~AfAfBp@jAnMnUx@nAfApAz@~@dA~@|B|AtAt@dEdBnDzA|CjAdCp@zAVlCX~AFhB@|GWtGW`CK`a@{AfPs@~BSbCYfBa@~Cy@dCy@lBw@pASdCs@xBo@`ASd@GXMFAPEvGk@x@G`Da@hJ}@tNeAhFg@zB]dB[zDkAxEyAfHsBjKgCjI}AdB_@dD{@R@PAnHuB|GoBh@Sb@Wr@o@\\e@`@w@Ng@TkAFe@AS?}@GqAUoCYoC_@_Bu@{CgAqFMo@YoCCg@B[N[RMXIt@Qn@hEpAfIc@?g@@}GnBQJMNGHY]uB{M" },
     {latlng: [10.423455, -75.544804],
      date: 'January 28, 2019',
      title: 'Cartagena de Indias',
      location: 'Cartagena, Bol&iacute;var, Colombia',
      url: 'https://www.schemebasedadventure.com/2019/01/28/cartagena-de-indias/',
      corner1: [10.412783, -75.528361],
-     corner2: [10.431804, -75.555225] }
+     corner2: [10.431804, -75.555225],
+     line: '' }
     // {latlng: [],
     //  date: '',
     //  title: '',
     //  location: '',
     //  url: '',
     //  corner1: [],
-    //  corner2: [] }
+    //  corner2: [],
+    //  line: '' }
 ];
+
+// HOW TO OBTAIN A POLYLINE
+// Visit the following Google Maps Directions API endpoint (typical location strings given; default mode is driving):
+// https://maps.googleapis.com/maps/api/directions/json?origin=37.840455,-122.257520&destination=Universal+Studios+Hollywood&key=API_KEY
+// Near the bottom, under "overview_polyline", copy the value associated with key "points"
+// Copy the whole string, including enclosing "", and leave any \ characters escaped with a second \
+// Replace the boilerplate '' with the copied string
